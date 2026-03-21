232b89447b7ee0cf49d735de032a21eaa2c1cf4b
Palmafox_git <david.kouassi.uottawa@gmail.com>
2026-03-20 22:56:13 -0400

    add utils

---
+++ utils/csv_export.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from Suspension_Simulation.analysis.amplitude import *
+# importing the data
+from Suspension_Simulation.optimizer import list_data
+
+# seperation list of values
+mass_data = []
+raideur_data = []
+friction_data = []
+for data in list_data :
+    mass_data.append(data[0])
+    raideur_data.append(data[1])
+    friction_data.append(data[2])
+
+# creation of the dataframe
+csv_data = {
+    "Mass" : mass_data,
+    "Raideur" : raideur_data,
+    "Friction" : friction_data,
+    "Amplitude" : amplitude_list,
+    "Time_Stop" : time_stop_list
+}
+
+df = pd.DataFrame(csv_data)
+df.to_csv("csv_data.csv")
+print(df)
