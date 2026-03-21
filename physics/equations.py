8b3cf5ab18933b90d14e19cfbc80a2ea65a3c35a
Palmafox_git <david.kouassi.uottawa@gmail.com>
2026-03-20 22:54:52 -0400

    add physcis

---
+++ physics/equations.py
@@ -0,0 +1,68 @@
+# import module
+
+#
+system_parameters = list_data[0]
+# function to define the equation
+def equation_sous_regime(tuple, pos, speed) : 
+    # computing of delta and zelta
+    delta = tuple[2] / (2 * np.sqrt(tuple[0] * tuple[1]))
+    pulsation = np.sqrt(tuple[1] / tuple[0])
+    zelta = pulsation * np.sqrt(1 - delta**2)
+    
+    # finding the value A and B 
+    A = pos 
+    B = (speed + delta * pulsation * pos) / zelta
+    
+    # setting the data
+    t = np.linspace(0, 2*np.pi, 500)
+    y = np.exp(-(delta * pulsation * t)) *  ((A * np.cos(zelta*t) + B * np.sin(zelta*t)))
+    return t,y
+
+def equation_critique_regime(tuple, pos, speed) :
+    # computing the pulsation value 
+    pulsation = np.sqrt(tuple[1] / tuple[0])
+    delta = tuple[2] / (2 * np.sqrt(tuple[0] * tuple[1]))
+    # finding the value A and B 
+    A = pos 
+    B = speed + pulsation * A
+    # value to compute
+    # approximation time to stop
+    settling_time = 4 / (delta * pulsation)
+    t_max = 5 * settling_time
+    t = np.arange(0, t_max, 0.01)
+    y = (A + B*t) * np.exp(-1 * pulsation * t)
+    return t, y
+        
+
+def equation_sur_regime(tuple, pos, speed) :
+    # computing of delta and zelta
+    delta = tuple[2] / (2 * np.sqrt(tuple[0] * tuple[1]))
+    pulsation = np.sqrt(tuple[1] / tuple[0])# computing of delta and zelta
+
+    # finding the initial values
+    r1 = (-pulsation) * (delta + np.sqrt(delta**2 - 1))
+    r2 = (-pulsation) * (delta - np.sqrt(delta**2 - 1))
+    A = pos - ((speed-2-pos*r1) / (-r1 + r2))
+    B = -A + pos
+
+    # value to compute
+    # approximation time to stop
+    settling_time = 4 / (delta * pulsation)
+    t_max = 5 * settling_time
+    t = np.arange(0, t_max, 0.01)
+    y = A * np.exp(r1*t) + B * np.exp(r2*t)
+    return t, y
+    
+    
+def equation_finder(response, system_parameters) :
+    # condtion base on the regime wanted
+    if response["regime_wanted"] == "sous" :
+        return equation_sous_regime(system_parameters, response["initial_position"], response["initial_speed"])
+
+    elif response["regime_wanted"] == "critique" :
+        return equation_critique_regime(system_parameters, response["initial_position"], response["initial_speed"])
+
+    elif response["regime_wanted"] == "sur" :
+        return equation_sur_regime(system_parameters, response["initial_position"], response["initial_speed"])
+    
+plotting_data = equation_finder(response, system_parameters)
